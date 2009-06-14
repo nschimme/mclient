d0a0c8adc6972df295a3a068af1e84b0238c9c60
nschimme <nschimme@962f5b74-7ec1-11dd-90a9-6306d3ffb32f>
2009-06-14 21:51:04 +0000

    Forgot to add the two new event files required for PluginSession
    git-svn-id: https://mclient-mume.googlecode.com/svn/trunk/mclient@176 962f5b74-7ec1-11dd-90a9-6306d3ffb32f

---
+++ src/mclientplugin/MClientEngineEvent.cpp
@@ -0,0 +1,62 @@
+#include "MClientEngineEvent.h"
+
+#include "MClientEventData.h"
+
+#include <QDebug>
+#include <QVariant>
+#include <QString>
+
+MClientEngineEvent::MClientEngineEvent(MClientEventData* payload,
+				       const MClientEngineEventDataType type,
+				       const QString session) 
+        : QEvent(QEvent::Type(10000)) {
+
+    _payload = QSharedDataPointer<MClientEventData>(payload);
+    _dataType = type;
+    _session = session;
+}
+
+
+MClientEngineEvent::MClientEngineEvent(MClientEventData* payload,
+				       const MClientEngineEventDataType type) 
+        : QEvent(QEvent::Type(10000)) {
+
+    // We take ownership of payload!
+    _payload = QSharedDataPointer<MClientEventData>(payload);
+    _dataType = type;
+}
+
+MClientEngineEvent::MClientEngineEvent(const MClientEngineEvent& me) 
+        : QEvent(QEvent::Type(10000)), _payload(me._payload),
+        _dataType(me._dataType), _session(me.session()) {
+  // Makes a copy of the event
+}
+
+
+MClientEngineEvent::~MClientEngineEvent() {
+}
+
+
+MClientEngineEventDataType MClientEngineEvent::dataType() const {
+    return _dataType;
+}
+
+
+const QString& MClientEngineEvent::session() const {
+    return _session;
+}
+
+
+void MClientEngineEvent::session(const QString s) {
+    _session = s;
+}
+
+
+QVariant* MClientEngineEvent::payload() {
+    return _payload->payload();
+}
+
+
+QSharedDataPointer<MClientEventData> MClientEngineEvent::shared() const {
+    return _payload;
+}
---
+++ src/mclientplugin/MClientEngineEvent.h
@@ -0,0 +1,47 @@
+#ifndef MCLIENTENGINEEVENT_H
+#define MCLIENTENGINEEVENT_H
+
+#include <QEvent>
+
+#include "MClientEventData.h"
+
+#include <QSharedDataPointer>
+#include <QStringList>
+
+class QVariant;
+class QSharedData;
+class QString;
+
+enum MClientEngineEventDataType
+  { EE_MANAGER_POST = 0, EE_DATATYPE_UPDATE
+  };
+
+class MClientEngineEvent : public QEvent {
+    
+    public:
+        MClientEngineEvent(MClientEventData* payload,
+			   const MClientEngineEventDataType type,
+			   const QString session);
+        MClientEngineEvent(MClientEventData* payload,
+			   const MClientEngineEventDataType type);
+        MClientEngineEvent(const MClientEngineEvent& me);
+        ~MClientEngineEvent();
+
+	// Setters
+	void session(const QString);
+	
+	// Getters
+        MClientEngineEventDataType dataType() const;
+        QVariant* payload();
+        QSharedDataPointer<MClientEventData> shared() const;
+	const QString& session() const;
+
+    protected:
+	MClientEngineEventDataType _dataType;
+        QSharedDataPointer<MClientEventData> _payload;
+        QString _session;
+	
+};
+
+
+#endif /* MCLIENTENGINEEVENT_H */
